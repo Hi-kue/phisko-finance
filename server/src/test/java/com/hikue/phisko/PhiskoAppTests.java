29203acb13230c56922c494ae88f495c6ed338c6
Muhammad Bilal Khan <lofi.audit@gmail.com>
2025-05-15 13:14:20 -0400

    add: base setup for /server

---
+++ server/src/test/java/com/hikue/phisko/PhiskoAppTests.java
@@ -0,0 +1,13 @@
+package com.hikue.phisko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhiskoAppTests {
+
+    @Test
+    void contextLoads() {
+        assert true;
+    }
+}
